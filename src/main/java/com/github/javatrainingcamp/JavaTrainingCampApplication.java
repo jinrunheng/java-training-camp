19d3dab6fb4ff380fb3f3373cd68910f69ce4736
jinrunheng <1175088275@qq.com>
2022-02-02 19:53:55 +0800

    init repo

---
+++ src/main/java/com/github/javatrainingcamp/JavaTrainingCampApplication.java
@@ -0,0 +1,13 @@
+package com.github.javatrainingcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTrainingCampApplication.class, args);
+    }
+
+}
